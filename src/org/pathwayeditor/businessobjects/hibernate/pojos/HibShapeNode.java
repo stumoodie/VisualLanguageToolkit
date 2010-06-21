e7acf364cb896b285e2d14185465e98ec8e3aa34
Stuart Moodie <stuart.moodie@eightpillars.uk.com>
2010-06-21 10:26:48 +0000

    Changed toString output so that it is less verbose and so easier to read in log messages.

--- src/org/pathwayeditor/businessobjects/hibernate/pojos/HibShapeNode.java
+++ src/org/pathwayeditor/businessobjects/hibernate/pojos/HibShapeNode.java
@@ -136,14 +136,14 @@ public class HibShapeNode extends HibCompoundNode implements IShapeNode {
 	@Override
 	public String toString(){
 		StringBuilder builder = new StringBuilder(this.getClass().getSimpleName());
-		builder.append("(model=");
-		builder.append(this.getModel());
-		builder.append(", index=");
+		builder.append("(index=");
 		builder.append(this.getIndex());
-		builder.append(", removed=");
+		builder.append(",uniqueIndex=");
+		builder.append(this.getUniqueIndex());
+		builder.append(",removed=");
 		builder.append(this.isRemoved());
-		builder.append(", attribute=");
-		builder.append(this.getAttribute());
+		builder.append(",attributeSerial=");
+		builder.append(this.getAttribute().getCreationSerial());
 		builder.append(")");
 		return builder.toString();
 	}
