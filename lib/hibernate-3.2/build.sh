b5e679efb6e3a3c1a509662fb05a77215c5d3342
Stuart Moodie <stuart.moodie@eightpillars.uk.com>
2010-09-03 20:57:14 +0000

    Reorganised libraries and removed unused libs.

--- lib/hibernate-3.2/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp "lib/ant-launcher-1.6.5.jar" org.apache.tools.ant.launch.Launcher -lib lib "$@"
--- lib/hsqldb/testrun/sqltool/noop.sql
+++
@@ -1,5 +0,0 @@
-/*
- * $Id: noop.sql,v 1.1 2007/08/09 03:28:37 unsaved Exp $
- *
- * Simplest test possible.  Does absolutely nothing.
- */
